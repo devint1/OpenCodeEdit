3587662159be9c4842db9174b623f6f3e71ffea0
Devin Tuchsen <devint1@icloud.com>
2014-12-07 17:55:53 -0600

    Add GPL and GPL notices

--- OpenCodeEdit/src/CodeStyleElement.h
+++ OpenCodeEdit/src/CodeStyleElement.h
@@ -1,9 +1,24 @@
 //
 //  CodeStyleElement.h
-//  CodeEdit
+//  OpenCodeEdit
 //
 //  Created by Devin Tuchsen on 10/14/14.
-//  Copyright (c) 2014 Devi Tuchsen. All rights reserved.
+//  Copyright (c) 2014 Devin Tuchsen. All rights reserved.
+//
+//  This file is part of OpenCodeEdit.
+//
+//  OpenCodeEdit is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  OpenCodeEdit is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <Cocoa/Cocoa.h>
